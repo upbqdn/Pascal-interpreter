8ff27480013322b194b8becae6e7becb2ab241fc
Raiper34 <raipergm34@gmail.com>
2014-11-20 01:58:33 +0100

    ups

---
+++ ial.h
@@ -0,0 +1,51 @@
+/*
+ *Hlavickovy subor pre ial.c
+ *Autor: Filip Gulan xgulan00@stud.fit.vutbr.cz
+ */
+
+/*
+ *Inkludovanie funkcii, ktore budeme pouzivat
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//#include <stdarg.h>
+
+/*
+ *Define na eliminovanie magickych konstant
+ */
+#define HASH_ARRAY_SIZE (10) //velkost hashovacej tabulky
+#define ORIGIN (0) //zaciatok hashovacej tabulky/ alebo proste 0
+#define CONTAINS (0) //obsahuje prvok/retazec...
+#define NOCONTAINS (1) //neobsahuje prvok/retazec
+#define NOSUBSTRING (-1) //neobsahuje podretazec
+
+/*
+ *Deklaracia struktur a premennych
+ */
+typedef struct elementS //struktura, ktora je prvok zoznamu
+{
+	struct elementS *ptr; //ukazatel na dalsi prvok
+	int token_state; //stav tokenu
+	char *token_data; //data tokenu
+} *list_element;
+
+typedef struct listS //struktura, ktora je zoznam
+{
+	list_element Act; //ukazatel na aktualny prvok s ktorym pracujeme, na akom mieste sa nachadzame 
+	list_element First; //ukazatel na prvy prvok zoznamu
+} list;
+
+extern list list_array[HASH_ARRAY_SIZE]; //globalna tabulka hash klucov
+
+/*
+ *Deklaracia funkcii
+ */
+ int hash(char *);
+ void hash_init();
+ void hash_insert(char *, int);
+ int hash_search(char *);
+ void hash_destroy();
+ void sort(char *, int, int);
+ int find(char *, char *);
+ void vector(char *, int *);
---
+++ stack.c
@@ -0,0 +1,84 @@
+/*
+ *Autor: Filip Gulan xgulan00@stud.fit.vutbr.cz
+ *Kniznica obsahujuca funkcie na pracu so zasobnikom.
+ */
+
+/*
+ *Nainkludovanie hackoveho suboru
+ */
+#include "stack.h"
+
+/*
+ *Premenne definicie
+ */
+stack S; //zasobnik
+
+/*
+ *Funkcia na inicializaciu zasobnika
+ *Parameter: Zasobnik
+ */
+void stack_init(stack *S)
+{
+	S->First = NULL;
+}
+
+/*
+ *Funkcia ktora ulozi na zasobnik hodnotu
+ *Parameter: Zasobnik a vkladana hodnota
+ */
+void myPush(stack *S ,int data)
+{
+	stack_element pomocna = malloc(sizeof(struct stack_elementS));
+	pomocna->data = data;
+	pomocna->ptr_next = S->First;
+	S->First = pomocna;
+}
+
+/*
+ *Funkcia na popnutie vrcholu zasobniku
+ *Parameter: Zasobnik
+ */
+void myPop(stack *S)
+{
+	stack_element pomocna = S->First;
+	S->First = S->First->ptr_next;
+	free(pomocna);
+}
+
+/*
+ *Funkcia vrati hodnotu vrcholu zasobniku
+ *Parameter: Zasobnik
+ */
+int myTop(stack *S)
+{
+	return S->First->data;
+}
+
+/*
+ *Zrusi cely Zasobnik
+ *Parameter: Zasobnik
+ */
+void destroyStack(stack *S)
+{
+	stack_element pomocna;
+	while(S->First != NULL)
+	{
+		pomocna = S->First;
+		S->First = S->First->ptr_next;
+		free(pomocna);
+	}
+}
+
+
+/*int main()
+{
+	stack_init(&S);
+	myPush(&S, 5);
+	myTop(&S);
+	myPush(&S, 6);
+	myTop(&S);
+	myPop(&S);
+	myTop(&S);
+	destroyStack(&S);
+ 	return 0;
+}*/
