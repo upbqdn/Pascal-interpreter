a27618d4001348348704d50d3dd27dfaf2637377
Raiper34 <raipergm34@gmail.com>
2014-11-24 23:54:34 +0100

    wtf

---
+++ buildin.c
@@ -0,0 +1,29 @@
+/*
+ *Autor: Filip Gulan xgulan00@stud.fit.vutbr.cz
+ *Kniznica obsahujuca funkcie na pracu so zasobnikom.
+ */
+
+/*
+ *Nainkludovanie hackoveho suboru
+ */
+#include "stack.h"
+
+/*
+ *Premenne definicie
+ */
+stack S; //zasobnik
+
+
+
+
+/*int main()
+{
+	stack_init(&S);
+	myPushMul(&S ,8, 1,2,3,4,5,80,75,87);
+	myPush(&S, 505);
+	myPush(&S, 7805);
+	myPush(&S, 55);
+	showStack(&S);
+	destroyStack(&S);
+ 	return 0;
+}*/
