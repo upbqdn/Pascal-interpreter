e180de435875cbe464a57875ce3a75fee21ff106
xrybar04 <xrybar04@stud.fit.vutbr.cz>
2014-11-25 19:29:35 +0100

    kostra interpreteru

---
+++ interpreter.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "IAL.h"
+#include "header.h"
+
+
+int inter() // doplnit predavanie
+{
+	while(1)
+	{
+      
+      switch() // doplnit
+      {
+        case I_WRITE:
+             printf("%s\n", );   // doplnit
+
+        break;
+        
+        default:  
+                  // tuto bude asi chybicka nejaka 
+        break  
+
+
+      }
+
+
+	}
+}
