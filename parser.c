88c61bcefe8f6305c753e5003c61775e345c510c
Filip Ježovica <xjezov01@stud.fit.vutbr.cz>
2014-11-25 23:39:28 +0100

    Update parser.c

--- parser.c
+++ parser.c
@@ -1,6 +1,6 @@
 /*
 -----------------------------------------------------
- *@Author: Marek Bielik		xbieli05@stud.fit.vutbr.cz
+  @Author: Marek Bielik		xbieli05@stud.fit.vutbr.cz
   @Author: Filip Gulan		xgulan00@stud.fit.vutbr.cz
   @Author: Filip Ježovica	xjezov01@stud.fit.vutbr.cz
   @Author: Luboš Matouška	xmatus29@stud.fit.vutbr.cz
