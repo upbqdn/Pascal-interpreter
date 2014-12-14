48f6f5d55781ea448139567343be5db53b5d3506
Filip Gulán <xgulan00@stud.fit.vutbr.cz>
2014-12-14 23:57:12 +0100

    a

--- astack.h
+++ astack.h
@@ -1,7 +1,12 @@
-/*
- *Hlavickovy subor pre stack.c
- *Autor: Filip Gulan xgulan00@stud.fit.vutbr.cz
- */
+/*                @Project: IFJ14
+-----------------------------------------------------
+  @Author: Marek Bielik   xbieli05@stud.fit.vutbr.cz
+  @Author: Filip Gulan    xgulan00@stud.fit.vutbr.cz
+  @Author: Filip Ježovica xjezov01@stud.fit.vutbr.cz
+  @Author: Luboš Matuška  xmatus29@stud.fit.vutbr.cz
+  @Author: Eduard Rybár   xrybar04@stud.fit.vutbr.cz
+-----------------------------------------------------
+*/
 
 /*
  *Inkludovanie funkcii
